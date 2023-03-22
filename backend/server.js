3a6bced389d2a25e051a740118b018b1e9fb16f1
Abdul Ahad <hadi@Haadis-MacBook-Air.local>
2023-03-22 18:14:29 +0530

    port set to 8000

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ const connectDB = require('./config/db')
 const cors = require('cors')
 const bodyParser = require('body-parser')
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 8000
 
 connectDB()
 
